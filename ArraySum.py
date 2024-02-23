3f9f71b4f48c2cabdaa157103c53c23eec3e8e98
ambuj152 <ambuj152@gmail.com>
2024-02-23 22:44:36 +0530

    #

---
+++ ArraySum.py
@@ -0,0 +1,14 @@
+def ArraySum(array):
+    sum=0
+    length=len(array)
+    for i in range(0,length):
+        sum=sum+i
+        return sum
+    print("the sum is",sum)   
+
+
+
+
+
+array=[1,2,3,4,5,]
+ArraySum(array)
--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,7 @@ dict={
     "Age":24,
     "spec":"btech",
 }
-'''print(dict["Age"])
-print(dict["spec"])'''
+
 for i in dict:
     print(i)
 for i in dict:
