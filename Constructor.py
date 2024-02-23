b97e9c667f9b2e4fb0bb360dfce0e21db7782315
ambuj152 <ambuj152@gmail.com>
2024-02-24 00:53:42 +0530

    opps

---
+++ Constructor.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self,fullname,age):
+        self.name=fullname
+        self.age=age
+
+s1=Student("Ambuj",24)
+print(s1.name)
+print(s1.age)
+
+s2=Student("Ankit",25)
+print(s2.name)
+print(s2.age)
