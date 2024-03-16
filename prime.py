9d0a062366a1fd9fcea838f110d3eac46213443d
ambuj152 <ambuj152@gmail.com>
2024-03-16 22:14:03 +0530

    #

--- prime.py
+++ prime.py
@@ -1,13 +1,16 @@
-num = 19
-flag = 0
-if num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            flag = 1
-            break
-    if flag:
+def prime():
+    num = 15
+    flag = 0
+    if num == 1:
         print(num, "is not a prime number")
-    else:
-        print(num, "is a prime number")
+    elif num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = 1
+                break
+        if flag:
+            print(num, "is not a prime number")
+        else:
+            print(num, "is a prime number")
+
+prime()
